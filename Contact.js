c19c5def70bcf4f4ad820a634d978308b9c73818
Shivam Jha <108573467+ShivamKumarJha2004@users.noreply.github.com>
2024-05-24 20:14:26 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Feel free to contact me at: <a href="mailto: Jha24978@gmail.com">Email Me</a></p>
+    </section>
+  );
+}
+
+export default Contact;
