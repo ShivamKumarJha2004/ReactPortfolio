f9c23cb3071ef1f9880f485f4548e5616a9dbc5e
Shivam Jha <108573467+ShivamKumarJha2004@users.noreply.github.com>
2024-05-24 20:19:09 +0530

    Create Project.js

---
+++ Project.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Project = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <div>
+        <h3>Hospital Management System</h3>
+        <p>This Project Manges all the Hospital work like Opening Account, Balance details,patiendet, Doctor appointment date  .This project manage all the data integrity and automation of work.
+         tech stack is used frontend JAVA and backend  MySQL for database.
+                                  <br></br> Hosted Link <a href="https://github.com/ShivamKumarJha2004/Hospital_Mangement_Systemm">click here to check</a> 
+        </p>
+      </div>
+      <div>
+        <h3>Bank Management System</h3>
+        <p>This Project Manges all the Bank work like balance detail, credit debit transfer of money from one account to another, generate new customer ID for .This project manage all the data integrity and automation of work.
+         tech stack is used frontend JAVA and backend  MySQL for database.
+                                  <br></br> Hosted Link  Hosted Link <a href=" https://github.com/ShivamKumarJha2004/Banking_Mangement_System">click here to check</a> 
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Project;
