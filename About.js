746f07bfee31b782ec940cdd4faad6d984cc8fb5
Shivam Jha <108573467+ShivamKumarJha2004@users.noreply.github.com>
2024-05-24 20:06:56 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p> I am Shivam Kumar Jha. I am Passionated Full Stack Software developer 🚀 having experience of building Web and Mobile Applications With Javascript/React/Node js/React Native and Some other collest library and framework .</p>
+   
+    </section>
+  );
+}
+
+export default About;
